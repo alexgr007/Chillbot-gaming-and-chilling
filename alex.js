dfc2e262269250c5dee2a5b240e86ad170cb26ea
alexgr007 <62437255+alexgr007@users.noreply.github.com>
2021-06-09 15:35:01 +0300

    Update and rename Luna.js to alex.js

--- alex.js
+++ alex.js
@@ -1,15 +1,9 @@
-/*     __                      ____        __
-      / /   __  ______  ____ _/ __ )____  / /_
-     / /   / / / / __ \/ __ `/ __  / __ \/ __/
-    / /___/ /_/ / / / / /_/ / /_/ / /_/ / /_
-   /_____/\__,_/_/ /_/\__,_/_____/\____/\__/
-
-   — An adorable Discord bot.           <3
-   —— Free, Open Source and Cross Platform                                    */
+/*  
+                                   */
 
 // ██████ Integrations █████████████████████████████████████████████████████████
 
-const Luna = require("./Structures/Luna");
+const luna = require("./Structures/Luna");
 
 // ██████ Initialization ███████████████████████████████████████████████████████
 
